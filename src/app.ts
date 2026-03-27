74a5b8a4581db5569ed7aa2987c20752eb1b4172
YUS / PIS <yusrilhafis@gmail.com>
2026-03-27 22:39:32 +0700

    fix: vercel deployment setup

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import { errorHandler } from './middlewares/error.middleware';
-import authRoutes from "./routes/auth.route"
-import userRoutes from "./routes/user.route"
-import taskRoutes from "./routes/task.route"
+import authRoutes from "./routes/auth.route";
+import userRoutes from "./routes/user.route";
+import taskRoutes from "./routes/task.route";
 
 dotenv.config();
 
@@ -13,13 +13,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(authRoutes)
-app.use(userRoutes)
-app.use(taskRoutes)
+app.use(authRoutes);
+app.use(userRoutes);
+app.use(taskRoutes);
 
-
-app.use(errorHandler)
-
+// debug log
 app.use((req, _res, next) => {
   console.log("HIT:", req.method, req.url);
   next();
@@ -30,8 +28,6 @@ app.get("/", (req, res) => {
   res.send("API RUNNING");
 });
 
+app.use(errorHandler);
 
-const PORT = Number(process.env.PORT) || 4000;
-app.listen(PORT, () => {
-  console.log(`✅ Service running on port ${PORT}`);
-});
+export default app; // 🔥 penting
